772176c06ebb7b2c7329845a0753015dcc74aeb9
Lim Yee Han <limyeehan@gmail.com>
2023-04-09 14:23:27 +0800

    location and speech2text

---
+++ location.py
@@ -0,0 +1,53 @@
+import logging
+import os
+import time
+from pprint import pprint
+
+import requests
+import telebot
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+
+# Get the bot token from .env file
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+# Enable TeleBot logging
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+
+# Handle location messages
+@bot.message_handler(content_types=['location'])
+def handle_location(message):
+    chat_id = message.chat.id
+    # Define the URL for the request
+    url = f"https://developers.onemap.sg/privateapi/commonsvc/revgeocode?location={message.location.latitude},{message.location.longitude}&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEwMTYxLCJ1c2VyX2lkIjoxMDE2MSwiZW1haWwiOiJsaW15ZWVoYW5AZ21haWwuY29tIiwiZm9yZXZlciI6ZmFsc2UsImlzcyI6Imh0dHA6XC9cL29tMi5kZmUub25lbWFwLnNnXC9hcGlcL3YyXC91c2VyXC9zZXNzaW9uIiwiaWF0IjoxNjgwOTczNDQxLCJleHAiOjE2ODE0MDU0NDEsIm5iZiI6MTY4MDk3MzQ0MSwianRpIjoiMzdjNWZhNjM2M2M5NDZmZTI5ZGNlZjY1MDA2ZWFlZDAifQ.WaefQMWiWZKU9QJG1GJvGZ0Mo-l0aJZwOF4tI14Ren8"
+
+    # Make the GET request
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON data from the response
+        data = response.json()
+
+        return_string = []
+        return_string.append("You are at:\n")
+
+        for result in data["GeocodeInfo"]:
+            return_string.append(
+                f"{result['BLOCK']} {result['ROAD']} {result['BUILDINGNAME']}, S({result['POSTALCODE']})\n")
+    else:
+        print(f"Error: {response.status_code}")
+
+    result = "".join(return_string)
+
+    bot.reply_to(
+        message, "".join(result)
+    )
+
+
+bot.polling()
---
+++ speech2text.py
@@ -0,0 +1,79 @@
+import logging
+import os
+import time
+from pprint import pprint
+
+import requests
+import telebot
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+
+# Get the bot token from .env file
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+# Enable TeleBot logging
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+
+def send_long_message(bot, chat_id, text, max_length=4096):
+    if len(text) <= max_length:
+        bot.send_message(chat_id, text)
+    else:
+        parts = []
+        while len(text) > 0:
+            if len(text) > max_length:
+                part = text[:max_length]
+                last_space = part.rfind(' ')
+
+                if last_space != -1:
+                    part = part[:last_space]
+
+                parts.append(part)
+                text = text[len(part):]
+            else:
+                parts.append(text)
+                break
+
+        for part in parts:
+            bot.send_message(chat_id, part)
+
+
+# Handle voice messages
+@bot.message_handler(content_types=['voice'])
+def handle_voice_message(message):
+    bot.send_message(message.chat.id, "Transcribing...")
+
+    file_info = bot.get_file_url(message.voice.file_id)
+
+    endpoint = "https://api.assemblyai.com/v2/transcript"
+    json = {
+        "audio_url": file_info
+    }
+    headers = {
+        "authorization": "f0d2f3df22fc499e90085f96f902b3cd",
+    }
+    response = requests.post(endpoint, json=json, headers=headers)
+
+    # print("@@@@@@@@@@@@@@", response.json())
+
+    status = ""
+    text = ""
+    endpoint = f"https://api.assemblyai.com/v2/transcript/{response.json()['id']}"
+    headers = {
+        "authorization": "f0d2f3df22fc499e90085f96f902b3cd",
+    }
+    while status != "completed":
+        response = requests.get(endpoint, headers=headers)
+        status = response.json()['status']
+        text = response.json()['text']
+        # wait 2 seconds
+        time.sleep(2)
+
+    send_long_message(bot, message.chat.id, text)
+
+
+bot.polling()
